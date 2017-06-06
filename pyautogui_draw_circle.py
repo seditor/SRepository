81d7a6323c0aab6e08c02bbbba083ab9aef6d8fa
seditor <seditor.wong@gmail.com>
2017-06-06 23:35:51 +0800

    Update pyautogui_draw_circle.py

--- pyautogui_draw_circle.py
+++ pyautogui_draw_circle.py
@@ -8,15 +8,15 @@ import math
  
 width, height = pyautogui.size()
  
-r = 250  # 圆的半径
-# 圆心
+r = 250  # radius
+# Center point
 o_x = width/2
 o_y = height/2
  
 pi = 3.1415926
  
-for i in range(10):   # 转10圈
-	for angle in range(0, 360, 5):  # 利用圆的参数方程
+for i in range(10):   # 10times
+	for angle in range(0, 360, 5):  # 360 degree
 		X = o_x + r * math.sin(angle*pi/180)
 		Y = o_y + r * math.cos(angle*pi/180)
  
